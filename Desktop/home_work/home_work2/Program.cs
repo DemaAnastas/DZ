983bc5afe44db060aeb384d21bbf1ec64bbc695f
Anastasia <nastushadem@mail.ru>
2023-12-21 13:55:52 +0500

    add home work 3

--- Desktop/home_work/home_work2/Program.cs
+++ Desktop/home_work/home_work2/Program.cs
@@ -2,45 +2,45 @@
 // программу, которая поменяет местами первую и
 // последнюю строку массива.
 
-int[,] array = new int[5, 4];
+// int[,] array = new int[5, 4];
 
-void CreateArray()
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
+// void CreateArray()
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
 
-void PrintArray()
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            System.Console.Write(array[i, j] + " ");
-        }
-        System.Console.WriteLine();
-    }
-    System.Console.WriteLine();
-}
+// void PrintArray()
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             System.Console.Write(array[i, j] + " ");
+//         }
+//         System.Console.WriteLine();
+//     }
+//     System.Console.WriteLine();
+// }
 
-void ChangeArray()
-{
-    for (int i = 0; i < array.GetLength(1); i++)
-    {
-        int temp = array [0,i];
-        array [0,i] = array [ array.GetLength(0) -1, i];
-        array [ array.GetLength(0) -1, i] = temp;
-    }
+// void ChangeArray()
+// {
+//     for (int i = 0; i < array.GetLength(1); i++)
+//     {
+//         int temp = array [0,i];
+//         array [0,i] = array [ array.GetLength(0) -1, i];
+//         array [ array.GetLength(0) -1, i] = temp;
+//     }
    
-}
+// }
 
-CreateArray();
-PrintArray();
-ChangeArray();
-PrintArray();
+// CreateArray();
+// PrintArray();
+// ChangeArray();
+// PrintArray();
 
